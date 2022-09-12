c0fd0d5388243f28156f8164ccd946b2cee520a1
kent <s9n9201@yahoo.com.tw>
2022-09-12 16:38:48 +0800

    Initial commit

---
+++ src/main/java/com/demo/newcoindesk/NewCoinDeskApplication.java
@@ -0,0 +1,13 @@
+package com.demo.newcoindesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewCoinDeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewCoinDeskApplication.class, args);
+    }
+
+}
